e9b8ef20e0bc8737182866451196ed61e6f9b79e
ist425453 <nelton.nunes@gmail.com>
2020-11-17 13:57:53 -0800

    84153

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -5,9 +5,22 @@
 
 int main()
 {
+int i=0; // iterador
+int value= 0; //valor random 
+int max_value= 0; // guarda o valor max
+
   //init uart 
-  uart_init(UART_BASE,FREQ/BAUD);   
-  uart_printf("\n\n\nHello world!\n\n\n");
+  uart_init(UART_BASE,FREQ/BAUD); 
+    
+    for (i = 0; i <= 100; ++i) {
+        value = rand();
+	uart_printf("Valor: %d\n", value);
+	if (value > max_value){
+	   max_value = value;
+			      }
+	uart_printf("Valor max: %d\n", max_value);
+}
+
   //char *a = malloc(10);
   //free(a);
 }
